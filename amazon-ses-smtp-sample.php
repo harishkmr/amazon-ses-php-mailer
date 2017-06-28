ebef877409ddba57e255c3294b20083f0ea36c4e
Harish Kumar <hari143is@gmail.com>
2017-06-28 11:30:10 +0530

    first commit

---
+++ amazon-ses-smtp-sample.php
@@ -0,0 +1,35 @@
+<?php
+
+require './vendor/autoload.php';
+
+$mail = new PHPMailer;
+
+$mail->isSMTP();
+
+$mail->Host = '';
+
+$mail->SMTPAuth = true;
+$mail->Username = '';
+$mail->Password = '';
+$mail->SMTPSecure = 'ssl';
+$mail->Port = 465;
+$mail->setFrom('from@domain.com', 'PACCAR');
+$mail->addAddress('toname@domain.com', 'Harisha V');
+
+$mail->isHTML(true);
+$mail->Subject = 'Amazon SES test (SMTP interface accessed using PHP)';
+$mail->Body    = '<h1>Email Test</h1>
+							    <p>This email was sent through the 
+							    <a href="http://aws.amazon.com/ses/">Amazon SES</a> SMTP
+							    interface using the <a href="https://github.com/PHPMailer/PHPMailer">
+							    PHPMailer</a> class.</p>';
+
+$mail->AltBody = "Email Test\r\nThis email was sent through the 
+Amazon SES SMTP interface using the PHPMailer class.";
+
+if(!$mail->send()) {
+    echo 'Email not sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Email sent!';
+}
